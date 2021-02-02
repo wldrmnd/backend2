35cfbe4c1f264c6bd562e0794f92e7ee43fc415f
Egor Barkovsky <woldermind@gmail.com>
2021-02-02 17:49:29 +0300

    initial commit

---
+++ src/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+    public static void main(String[] args) {
+        SecondBackendGroupChecker checker = new SecondBackendGroupChecker();
+        System.out.println(checker.isNameExists("женя"));
+    }
+}
